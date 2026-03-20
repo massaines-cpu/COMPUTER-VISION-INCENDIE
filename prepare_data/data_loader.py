8e8000c808aa4bc84c283d5d85410c161bc8f764
inesmassa <inesurvivor@gmail.com>
2026-03-20 16:50:38 +0100

    fin 1ere tache

--- prepare_data/data_loader.py
+++ prepare_data/data_loader.py
@@ -1,2 +1,33 @@
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+import json
+import pandas as pd
+from PIL import Image
+
+
 #écrire une fonction qui ouvre ce JSON avec json.load()
+
+path = '../data/_annotations.coco.json'
+def open_data(path):
+    with open (path) as f:
+        coco = json.load(f)
+
+    return coco
+
+coco = open_data(path)
+
+
 #Écrire une 2e fonction qui charge images et annotations dans des DataFrames pandas
+
+df_images = pd.DataFrame(coco['images'])
+df_annotations = pd.DataFrame(coco['annotations'])
+
+def dataframe_load(coco):
+    df_images = pd.DataFrame(coco['images'])
+    df_annotations = pd.DataFrame(coco['annotations'])
+    return df_images, df_annotations
+
+print(df_images, df_annotations)
+
+
+
---
+++ prepare_data/test_manar_ines.py
@@ -0,0 +1,33 @@
+from matplotlib import pyplot as plt
+import matplotlib.patches as patches
+import json
+import pandas as pd
+from PIL import Image
+
+#écrire une fonction qui ouvre ce JSON avec json.load()
+
+with open ('../data/_annotations.coco.json') as f:
+    coco = json.load(f)
+plt.rcParams['figure.figsize'] = (15, 15)
+
+image_id = None
+image_name = '../data/cl6e3e9qm003rgk55efdu14ah_2_FALSE_COLOR_jpg.rf.8abfdde94619a88574680bdb5bafa4dc.jpg'
+
+for image in coco['images']:
+    if image['file_name'] == image_name.split('/')[-1]:
+        image_id = image['id']
+        break
+
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 15))
+img = Image.open(image_name)
+ax.imshow(img)
+
+for image_ann in coco['annotations']:
+    if image_ann['image_id'] == image_id:
+        x, y, w, h = image_ann['bbox']
+        bb = patches.Rectangle((x, y), w, h, linewidth=3, edgecolor='red', facecolor='none')
+        ax.add_patch(bb)
+
+
+plt.show()
