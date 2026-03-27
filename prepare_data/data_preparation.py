7e3cb969be023b3115bd9b620342af6967a5627c
inesmassa <inesurvivor@gmail.com>
2026-03-27 13:45:27 +0100

    split

--- prepare_data/data_preparation.py
+++ prepare_data/data_preparation.py
@@ -37,8 +37,8 @@ for f in os.listdir(data):
 
 print(liste_images)
 
-count = 0
-
+# count = 0
+#
 # for i in liste_images:
 #     fig, ax = plt.subplots(1, 1, figsize=(15, 15))
 #     img = Image.open(data + i)
@@ -111,4 +111,30 @@ for f in os.listdir(data):
 print(liste_images_path)
 
 import shutil
+from sklearn.model_selection import train_test_split
+train, temp = train_test_split(liste_images_path, test_size=0.3, random_state=42)
+val, test = train_test_split(temp, test_size=0.33, random_state=42)
 
+print(f"Train : {len(train)} images")
+print(f"Val : {len(val)} images")
+print(f"Test : {len(test)} images")
+
+def copier_fichiers(liste, dossier_destination):
+
+    for image_path in liste:
+        # Copier l'image
+        shutil.copy(image_path, dossier_destination / 'images')
+
+        # Trouver et copier le label associé
+        nom_label = image_path.stem + '.txt'  # même nom mais .txt
+        label_path = Path('../data/labels') / nom_label
+
+        if label_path.exists():
+            shutil.copy(label_path, dossier_destination / 'labels')
+
+    # Appliquer la fonction sur chaque split
+copier_fichiers(train, Path('../data/train'))
+copier_fichiers(val, Path('../data/val'))
+copier_fichiers(test, Path('../data/test'))
+
+print("Split terminé !")
