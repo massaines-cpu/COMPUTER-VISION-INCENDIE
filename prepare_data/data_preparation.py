7808db12581a60b8d593a33f2508f01d83fe1ebc
inesmassa <inesurvivor@gmail.com>
2026-03-27 13:57:47 +0100

    sans fiftyone

--- prepare_data/data_preparation.py
+++ prepare_data/data_preparation.py
@@ -1,3 +1,4 @@
+from Cryptodome.SelfTest.Cipher.test_CFB import file_name
 from matplotlib import pyplot as plt
 import supervision as sv
 from pathlib import Path
@@ -35,20 +36,35 @@ for f in os.listdir(data):
     if '.jpg' in f:
         liste_images.append(f)
 
-print(liste_images)
+print('liste images', liste_images)
 
-# count = 0
-#
-# for i in liste_images:
-#     fig, ax = plt.subplots(1, 1, figsize=(15, 15))
-#     img = Image.open(data + i)
-#     ax.imshow(img)
-#     ax.axis('off')
-#
-#     count += 1
-#     if count % 20 == 0:
-#         plt.show()
-# # plt.show()
+count = 0
+with open ('../data/_annotations.coco.json') as f:
+    coco = json.load(f)
+plt.rcParams['figure.figsize'] = (15, 15)
+
+for i in liste_images:
+    image_id = None
+    for img_info in coco['images']:
+        if img_info['file_name'] == i:
+            image_id = img_info['id']
+            break
+
+    fig, ax = plt.subplots(1, 1, figsize=(15, 15))
+    img = Image.open(data + i)
+    ax.imshow(img)
+    ax.axis('off')
+
+    for image_ann in coco['annotations']:
+        if image_ann['image_id'] == image_id:
+            x, y, w, h = image_ann['bbox']
+            bb = patches.Rectangle((x, y), w, h, linewidth=3, edgecolor='red', facecolor='none')
+            ax.add_patch(bb)
+
+    count += 1
+    if count % 20 == 0:
+        plt.show()
+plt.show()
 
 # convertit les annotations au format YOLO
 
